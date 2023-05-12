392bc19b0a1af5370e7f756836e8df7a453e32c1
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-05-12 21:38:18 +0530

    four

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import MenuAppBar from "./Component/Header/Header";
 import "./App.css";
 import Navbar from "./Component/Navbar/NavBar"
 
-
+// import AddList from "./Component/AddList/AddList";
 const App = () => {
   return (
     <div className="app">
       <div className="app_navbar">
         <MenuAppBar />
         <Navbar />
+        {/* <AddList/> */}
       </div>
       <div className="app_outer">
         <div className="app_boards">
