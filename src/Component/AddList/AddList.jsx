92a49b5379378c4893750d14ee023b7e5bf7c632
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-05-15 15:09:30 +0530

    five

--- src/Component/AddList/AddList.jsx
+++ src/Component/AddList/AddList.jsx
@@ -1,17 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./AddList.module.css";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { nanoid } from "nanoid";
 
 export default function AddList() {
   const [isVisible, setIsVisible] = useState(true);
   const [isClicked, setIsClicked] = useState(false);
   const [listItems, setListItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  useEffect(() => {
+    const savedListItems = localStorage.getItem("listItems");
+    if (savedListItems) {
+      setListItems(JSON.parse(savedListItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+  }, [listItems]);
 
   const handleAddList = () => {
-    setIsClicked(true);
-    setListItems([...listItems, "New List Item"]);
+    if (newItem.trim() !== "") {
+      setListItems([
+        ...listItems,
+        { id: nanoid(), name: newItem, isDeleted: false },
+      ]);
+      setIsClicked(true);
+      setNewItem(""); // clear input after adding item
+    }
+  };
+
+  const handleDeleteItem = (id) => {
+    const updatedListItems = listItems.map((item) =>
+      item.id === id ? { ...item, isDeleted: true } : item
+    );
+    setListItems(updatedListItems);
+  };
+
+  const handleUndoDelete = (id) => {
+    const updatedListItems = listItems.map((item) =>
+      item.id === id ? { ...item, isDeleted: false } : item
+    );
+    setListItems(updatedListItems);
+  };
+
+  const handleClearList = () => {
+    setListItems([]);
   };
 
   return (
@@ -25,7 +62,11 @@ export default function AddList() {
         </div>
       ) : (
         <div className={styles.inputWrapper}>
-          <input type="text" />
+          <input
+            type="text"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
           <div className={styles.innerDiv}>
             <Button variant="contained" onClick={handleAddList}>
               Add List
@@ -35,10 +76,119 @@ export default function AddList() {
         </div>
       )}
       <ul>
-        {listItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
+        {listItems.map((item) => {
+          if (!item.isDeleted) {
+            return (
+              <li key={item.id}>
+                {item.name}{" "}
+                <span onClick={() => handleDeleteItem(item.id)}></span>
+              </li>
+            );
+          } else {
+            return (
+              <li key={item.id} className={styles.deletedItem}>
+                {item.name}
+                <span onClick={() => handleUndoDelete(item.id)}></span>
+              </li>
+            );
+          }
+        })}
       </ul>
+      <div className={styles.btnWrapper} onClick={handleClearList}>
+        <span>Clear list</span>
+      </div>
     </div>
   );
 }
+
+// import { useState } from "react";
+// import styles from "./AddList.module.css";
+// import AddIcon from "@mui/icons-material/Add";
+// import { Button } from "@mui/material";
+// import CloseIcon from "@mui/icons-material/Close";
+// import { nanoid } from "nanoid";
+
+// export default function AddList() {
+//   const [isVisible, setIsVisible] = useState(true);
+//   const [isClicked, setIsClicked] = useState(false);
+//   const [listItems, setListItems] = useState([]);
+//   const [newItem, setNewItem] = useState("");
+
+//   const handleAddList = () => {
+//     if (newItem.trim() !== "") {
+//       setListItems([
+//         ...listItems,
+//         { id: nanoid(), name: newItem, isDeleted: false },
+//       ]);
+//       setIsClicked(true);
+//       setNewItem(""); // clear input after adding item
+//     }
+//   };
+
+//   const handleDeleteItem = (id) => {
+//     const updatedListItems = listItems.map((item) =>
+//       item.id === id ? { ...item, isDeleted: true } : item
+//     );
+//     setListItems(updatedListItems);
+//   };
+
+//   const handleUndoDelete = (id) => {
+//     const updatedListItems = listItems.map((item) =>
+//       item.id === id ? { ...item, isDeleted: false } : item
+//     );
+//     setListItems(updatedListItems);
+//   };
+
+//   const handleClearList = () => {
+//     setListItems([]);
+//   };
+
+//   return (
+//     <div
+//       className={styles.container}
+//       style={{ backgroundColor: isClicked ? "white" : "" }}
+//     >
+//       {isVisible ? (
+//         <div className={styles.btnWrapper} onClick={() => setIsVisible(false)}>
+//           <AddIcon /> <span> Add another list</span>
+//         </div>
+//       ) : (
+//         <div className={styles.inputWrapper}>
+//           <input
+//             type="text"
+//             value={newItem}
+//             onChange={(e) => setNewItem(e.target.value)}
+//           />
+//           <div className={styles.innerDiv}>
+//             <Button variant="contained" onClick={handleAddList}>
+//               Add List
+//             </Button>
+//             <CloseIcon onClick={() => setIsVisible(true)} />
+//           </div>
+//         </div>
+//       )}
+//       <ul>
+//         {listItems.map((item) => {
+//           if (!item.isDeleted) {
+//             return (
+//               <li key={item.id}>
+//                 {item.name}{" "}
+//                 <span onClick={() => handleDeleteItem(item.id)}></span>
+//               </li>
+//             );
+//           } else {
+//             return (
+//               <li key={item.id} className={styles.deletedItem}>
+//                 {item.name}
+//                 <span onClick={() => handleUndoDelete(item.id)}></span>
+//               </li>
+//             );
+//           }
+//         })}
+//       </ul>
+//       <div className={styles.btnWrapper} onClick={handleClearList}>
+//         <span>Clear list</span>
+//       </div>
+//     </div>
+//   );
+// }
--- src/Component/List/Card/Card.jsx
+++ src/Component/List/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import "./Card.css";
 
 import EditIcon from "@mui/icons-material/Edit";
