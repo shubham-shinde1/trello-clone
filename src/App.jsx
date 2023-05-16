04f6c0613be46fafd55a0dea496cad314095da64
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-05-16 14:55:51 +0530

    Six

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,304 @@
+// import Board from "./Component/List/Board/Board";
+// import Editable from "./Component/List/Editable/Editable";
+// import BoardAppBar from "./Component/Header/Header";
+// import "./App.css";
+// <<<<<<< HEAD
+// import Navbar from "./Component/Navbar/NavBar"
+// =======
+// import Discription from "./Component/discription/Discription";
+// import CardDetails from "./Component/discription/Discription";
+// >>>>>>> e1f8561984aa77da5549417adb7f2d6258662d0a
+
+// import AddList from "./Component/AddList/AddList";
+// const App = () => {
+//   return (
+// <<<<<<< HEAD
+//     <div className="app">
+//       <div className="app_navbar">
+//         <Navbar />
+//         <MenuAppBar />
+
+//         <AddList />
+//       </div>
+// =======
+//     <>
+//       {/* //  <Discription />
+//     // <div className="app">
+//     //   <div className="app_navbar"> */}
+//       {/* <BoardAppBar /> */}
+//       <CardDetails />
+//       {/* <Navbar /> */}
+//       {/* </div>
+// >>>>>>> e1f8561984aa77da5549417adb7f2d6258662d0a
+//       <div className="app_outer">
+//         <div className="app_boards">
+//           <Board />
+
+//           <Editable />
+//         </div>
+//       </div>
+//     </div> */}
+//     </>
+//   );
+// };
+
+// export default App;
+// import Board from "./Component/List/Board/Board";
+// import Editable from "./Component/List/Editable/Editable";
+// // import BoardAppBar from "./Component/Header/Header";
+// import AddList from "./Component/AddList/AddList";
+// import "./App.css";
+// import Navbar from "./Component/Navbar/NavBar"
+
+// // import AddList from "./Component/AddList/AddList";
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <div className="app_navbar">
+//         <Navbar />
+//         {/* <MenuAppBar /> */}
+
+//         <AddList />
+//       </div>
+//       <div className="app_outer">
+//         <div className="app_boards">
+//           <Board />
+
+//           <Editable />
+//         </div>
+//       </div>
+//     </div> 
+  
+//   );
+// };
+
+// export default App;
+
+
+
+
+
 import Board from "./Component/List/Board/Board";
 import Editable from "./Component/List/Editable/Editable";
-import BoardAppBar from "./Component/Header/Header";
+import BoardAppBar from "./Component/Header/BoardAppBar";
 import "./App.css";
-<<<<<<< HEAD
-import Navbar from "./Component/Navbar/NavBar"
-=======
-import Discription from "./Component/discription/Discription";
-import CardDetails from "./Component/discription/Discription";
->>>>>>> e1f8561984aa77da5549417adb7f2d6258662d0a
 
-import AddList from "./Component/AddList/AddList";
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [boards, setBoards] = useState(
+    JSON.parse(localStorage.getItem("prac-kanban")) || []
+  );
+
+  const addboardHandler = (name) => {
+    const tempBoards = [...boards];
+    tempBoards.push({
+      id: Date.now() + Math.random() * 2,
+      title: name,
+      cards: [],
+    });
+    setBoards(tempBoards);
+  };
+
+  const removeBoard = (id) => {
+    const index = boards.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    tempBoards.splice(index, 1);
+    setBoards(tempBoards);
+  };
+
+  const addCardHandler = (id, title) => {
+    const index = boards.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    tempBoards[index].cards.push({
+      id: Date.now() + Math.random() * 2,
+      title,
+
+      date: "",
+    });
+    setBoards(tempBoards);
+  };
+
+  const removeCard = (bid, cid) => {
+    const index = boards.findIndex((item) => item.id === bid);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    const cards = tempBoards[index].cards;
+
+    const cardIndex = cards.findIndex((item) => item.id === cid);
+    if (cardIndex < 0) return;
+
+    cards.splice(cardIndex, 1);
+    setBoards(tempBoards);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("prac-kanban", JSON.stringify(boards));
+  }, [boards]);
+
   return (
-<<<<<<< HEAD
     <div className="app">
       <div className="app_navbar">
-        <Navbar />
-        <MenuAppBar />
-
-        <AddList />
+        <BoardAppBar />
       </div>
-=======
-    <>
-      {/* //  <Discription />
-    // <div className="app">
-    //   <div className="app_navbar"> */}
-      {/* <BoardAppBar /> */}
-      <CardDetails />
-      {/* <Navbar /> */}
-      {/* </div>
->>>>>>> e1f8561984aa77da5549417adb7f2d6258662d0a
+
       <div className="app_outer">
         <div className="app_boards">
-          <Board />
+          {boards.map((item) => (
+            <Board
+              key={item.id}
+              board={item}
+              addCard={addCardHandler}
+              removeBoard={() => removeBoard(item.id)}
+              removeCard={removeCard}
+            />
+          ))}
 
-          <Editable />
+          <div className="app_boards_board">
+            <Editable
+              displayClass="app_boards_add-board"
+              editClass="app_boards_add-board_edit"
+              placeholder="Enter Board Name"
+              text="Add Board"
+              buttonText="Add Board"
+              onSubmit={addboardHandler}
+            />
+          </div>
         </div>
       </div>
-    </div> */}
-    </>
+    </div>
   );
 };
 
 export default App;
+
+
+
+
+
+// // import Board from "./Component/List/Board/Board";
+// // import Editable from "./Component/List/Editable/Editable";
+// // import BoardAppBar from "./Component/Header/Header";
+// // import "./App.css";
+
+// // import { useState ,useEffect} from "react";
+
+// // import Discription from "./Component/discription/Discription";
+// // import CardDetails from "./Component/discription/Discription";
+
+
+// // // import Navbar from "./Component/navBar/NavBar";
+// // const App = () => {
+// //   const [boards, setBoards] = useState(
+// //     JSON.parse(localStorage.getItem("prac-kanban")) || []
+// //   );
+
+
+// //  const addboardHandler = (name) => {
+// //     const tempBoards = [...boards];
+// //     tempBoards.push({
+// //       id: Date.now() + Math.random() * 2,
+// //       title: name,
+// //       cards: [],
+// //     });
+// //     setBoards(tempBoards);
+// //   };
+
+ 
+// //   const removeBoard = (id) => {
+// //     const index = boards.findIndex((item) => item.id === id);
+// //     if (index < 0) return;
+
+// //     const tempBoards = [...boards];
+// //     tempBoards.splice(index, 1);
+// //     setBoards(tempBoards);
+// //   };
+
+// //   const addCardHandler = (id, title) => {
+// //     const index = boards.findIndex((item) => item.id === id);
+// //     if (index < 0) return;
+
+// //     const tempBoards = [...boards];
+// //     tempBoards[index].cards.push({
+// //       id: Date.now() + Math.random() * 2,
+// //       title,
+   
+// //       date: "",
+ 
+// //     });
+// //     setBoards(tempBoards);
+// //   };
+
+// //   const removeCard = (bid, cid) => {
+// //     const index = boards.findIndex((item) => item.id === bid);
+// //     if (index < 0) return;
+
+// //     const tempBoards = [...boards];
+// //     const cards = tempBoards[index].cards;
+
+// //     const cardIndex = cards.findIndex((item) => item.id === cid);
+// //     if (cardIndex < 0) return;
+
+// //     cards.splice(cardIndex, 1);
+// //     setBoards(tempBoards);
+// //   };
+
+// //   useEffect(() => {
+// //     localStorage.setItem("prac-kanban", JSON.stringify(boards));
+// //   }, [boards]);
+
+// //   return(
+    
+// //     <div className="app">
+    
+// //       <div className="app_navbar">
+// //         <MenuAppBar />
+// //         {/* <Navbar /> */}
+// //       </div>
+
+//     <>
+//       {/* //  <Discription />
+//     // <div className="app">
+//     //   <div className="app_navbar"> */}
+//       {/* <BoardAppBar /> */}
+//       {/* <CardDetails /> */}
+//       {/* <Navbar /> */}
+//       {/* {/* </div> */}
+
+//       {/* <div className="app_outer">
+//         <div className="app_boards">
+
+        // {boards.map((item) => (
+        //     <Board
+        //       key={item.id}
+        //       board={item}
+        //       addCard={addCardHandler}
+        //       removeBoard={() => removeBoard(item.id)}
+        //       removeCard={removeCard}
+        //     />
+        //   ))}
+
+
+//           <div className="app_boards_board">
+//           <Editable
+//               displayClass="app_boards_add-board"
+//               editClass="app_boards_add-board_edit"
+//               placeholder="Enter Board Name"
+//               text="Add Board"
+//               buttonText="Add Board"
+//               onSubmit={addboardHandler}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div> 
+//     </>
+//   );
+// };
+
+// export default App; */}
--- src/Component/AddList/AddList.jsx
+++ src/Component/AddList/AddList.jsx
@@ -101,94 +101,12 @@ export default function AddList() {
   );
 }
 
-// import { useState } from "react";
-// import styles from "./AddList.module.css";
-// import AddIcon from "@mui/icons-material/Add";
-// import { Button } from "@mui/material";
-// import CloseIcon from "@mui/icons-material/Close";
-// import { nanoid } from "nanoid";
 
-// export default function AddList() {
-//   const [isVisible, setIsVisible] = useState(true);
-//   const [isClicked, setIsClicked] = useState(false);
-//   const [listItems, setListItems] = useState([]);
-//   const [newItem, setNewItem] = useState("");
 
-//   const handleAddList = () => {
-//     if (newItem.trim() !== "") {
-//       setListItems([
-//         ...listItems,
-//         { id: nanoid(), name: newItem, isDeleted: false },
-//       ]);
-//       setIsClicked(true);
-//       setNewItem(""); // clear input after adding item
-//     }
-//   };
 
-//   const handleDeleteItem = (id) => {
-//     const updatedListItems = listItems.map((item) =>
-//       item.id === id ? { ...item, isDeleted: true } : item
-//     );
-//     setListItems(updatedListItems);
-//   };
 
-//   const handleUndoDelete = (id) => {
-//     const updatedListItems = listItems.map((item) =>
-//       item.id === id ? { ...item, isDeleted: false } : item
-//     );
-//     setListItems(updatedListItems);
-//   };
 
-//   const handleClearList = () => {
-//     setListItems([]);
-//   };
 
-//   return (
-//     <div
-//       className={styles.container}
-//       style={{ backgroundColor: isClicked ? "white" : "" }}
-//     >
-//       {isVisible ? (
-//         <div className={styles.btnWrapper} onClick={() => setIsVisible(false)}>
-//           <AddIcon /> <span> Add another list</span>
-//         </div>
-//       ) : (
-//         <div className={styles.inputWrapper}>
-//           <input
-//             type="text"
-//             value={newItem}
-//             onChange={(e) => setNewItem(e.target.value)}
-//           />
-//           <div className={styles.innerDiv}>
-//             <Button variant="contained" onClick={handleAddList}>
-//               Add List
-//             </Button>
-//             <CloseIcon onClick={() => setIsVisible(true)} />
-//           </div>
-//         </div>
-//       )}
-//       <ul>
-//         {listItems.map((item) => {
-//           if (!item.isDeleted) {
-//             return (
-//               <li key={item.id}>
-//                 {item.name}{" "}
-//                 <span onClick={() => handleDeleteItem(item.id)}></span>
-//               </li>
-//             );
-//           } else {
-//             return (
-//               <li key={item.id} className={styles.deletedItem}>
-//                 {item.name}
-//                 <span onClick={() => handleUndoDelete(item.id)}></span>
-//               </li>
-//             );
-//           }
-//         })}
-//       </ul>
-//       <div className={styles.btnWrapper} onClick={handleClearList}>
-//         <span>Clear list</span>
-//       </div>
-//     </div>
-//   );
-// }
+
+
+
